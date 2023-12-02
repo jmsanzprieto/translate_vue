1428e7211189036c85f2e749c2522c40f8a811ea
José Manuel Sanz <34507594+jmsanzprieto@users.noreply.github.com>
2023-12-02 08:08:06 +0100

    Create config.js

---
+++ vue_movies/src/logic/config.js
@@ -0,0 +1,16 @@
+// Configuraciones y endpoints
+// config.js
+
+// datos del usuario
+const username = 'USERNAME_API';
+const password = 'PASS_API';
+
+// Endpoints
+// ACCESO A LA AUTENTICACIÓN
+const API_AUTH = 'http://127.0.0.1:8000'; 
+
+// ACCESO A LOS DATOS DE LAS PELICULAS
+const API_MOVIES = 'http://127.0.0.1:8000/movies/movies/';
+
+// Exportaciones 
+export { username, password, API_AUTH, API_MOVIES };
